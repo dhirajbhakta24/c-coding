d7960ab81915b899f189af1e2f74e191e5068573
Dhiraj Kumar Bhakta <bhaktadhiraj@gmail.com>
2023-08-13 12:05:07 +0530

    second commit

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(int j=1; j<=5; j++)
+    {
+        for(i=1; i<=5; i++)
+        {
+            printf("%d",i);
+        }
+    printf("\n");
+    }
+}
+
